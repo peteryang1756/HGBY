dd8be4cf2cb9c90003eb6937ae6264831778c84a
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-24 20:42:31 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/**
- * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
- */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
