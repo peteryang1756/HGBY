e9a5b989980d4d4a1ecd454e21492f3edd50bcd9
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-24 19:48:46 +0800

    Update counters.tsx

--- components/counters.tsx
+++ components/counters.tsx
@@ -4,21 +4,53 @@ import { useState } from 'react'
 import styles from './counters.module.css'
 
 function MyButton() {
-  const [count, setCount] = useState(0)
-
-  function handleClick() {
-    setCount(count + 1)
-  }
-
   return (
-    <div>
-      <button onClick={handleClick} className={styles.counter}>
-        Clicked {count} times
-      </button>
+     <div className="{styles.header}">
+    <div className="{styles.container}">
+      <div className="{styles.hero-content}">
+        <div className="{styles.hero-block}">
+          <div className="{styles.content-block-hero}">
+            <h1 className="{styles.heading-2}">
+              <strong className="{styles.bold-text-9}">我們的</strong>
+              <span>
+                <strong className="{styles.bold-text}">使命</strong>
+              </span>
+              <strong className="{styles.bold-text-9}">是將</strong>
+              <span>
+                <strong className="{styles.bold-text-3}">雙</strong>
+              </span>
+              <strong className="{styles.bold-text-3}">龍</strong>
+              <span>
+                <strong className="{styles.bold-text-3}">體</strong>
+                <strong className="{styles.bold-text-3}">育</strong>
+              </span>
+              <strong className="{styles.bold-text-10}">組成</strong>
+              <span>
+                <strong className="{styles.bold-text-6}">最棒</strong>
+             </span>
+              <strong className="{styles.bold-text-10}">體育組織</strong>
+              <span>
+              </span>
+              <span className="{styles.cursor}">
+                <strong className="{styles.cursor}" data-owner="main"></strong>
+              </span>
+            </h1>
+            <div className="{styles.button-wrap-hero}">
+            </div>
+          </div>
+        </div>
+        <div className="{styles.hero-image-wrap}">
+          <img
+            alt=""
+            className="{styles.hero-image}"
+            src="https://74-rho.vercel.app/images/undraw_fitness_stats_sht6.svg"
+          />
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
 export default function MyApp() {
-  return <MyButton />
 }
